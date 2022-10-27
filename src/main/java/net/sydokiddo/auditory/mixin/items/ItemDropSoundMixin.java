eaedcc36e887a18567a7c92fd86ba7e3afd03b65
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-10-26 23:31:43 -0700

    Item drop sound no longer plays when switching dimensions or upon respawning

--- src/main/java/net/sydokiddo/auditory/mixin/items/ItemDropSoundMixin.java
+++ src/main/java/net/sydokiddo/auditory/mixin/items/ItemDropSoundMixin.java
@@ -30,8 +30,10 @@ public abstract class ItemDropSoundMixin extends LivingEntity {
     @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("RETURN"))
     public void drop(ItemStack itemStack, boolean bl, boolean bl2, CallbackInfoReturnable<ItemEntity> cir) {
 
-        if (Auditory.getConfig().misc_sounds.item_drop_sounds) {
-            this.playNotifySound(ModSoundEvents.ENTITY_PLAYER_DROP_ITEM, SoundSource.PLAYERS, 0.4F, 1.0F + level.random.nextFloat() * 0.4F);
+        if (!itemStack.isEmpty()) {
+            if (Auditory.getConfig().misc_sounds.item_drop_sounds) {
+                this.playNotifySound(ModSoundEvents.ENTITY_PLAYER_DROP_ITEM, SoundSource.PLAYERS, 0.4F, 1.0F + level.random.nextFloat() * 0.4F);
+            }
         }
     }
 }
